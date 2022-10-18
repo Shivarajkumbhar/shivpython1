4950df375f6f675550ad97fffff06ab19326b16f
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-10-18 20:58:17 +0530

    operatins on sets

---
+++ operations on sets.py
@@ -0,0 +1,9 @@
+countries=("india","japan","USA","canada","UK","shrilanka")
+print(countries)
+countries.add("austrelia","UAE")
+print(countries)
+ocean=("indian","pasific")
+countries.update(ocean)
+countries.remove("USA")
+countries.discard("xyz")
+countries.pop()  #randomly removing
