251566a5027421166ae78c6a56b94502469ab56c
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-10-18 20:15:01 +0530

    operations on list

---
+++ operation on list.py
@@ -0,0 +1,11 @@
+shiv=[1,3,5,12,9,78,12,89,200,100]
+shiv.sort()
+print(shiv)
+shiv.sort(reverse=True)
+print(shiv)
+shiv.reverse()
+print(shiv)
+print(len(shiv))
+print(shiv[9])
+shiv[9]=1000
+print(shiv)
