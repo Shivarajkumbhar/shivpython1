5e38f7437bed9f2e947f932b24b430cff6f27f37
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-10-18 20:23:47 +0530

    Update operation on list.py

--- operation on list.py
+++ operation on list.py
@@ -9,3 +9,17 @@ print(len(shiv))
 print(shiv[9])
 shiv[9]=1000
 print(shiv)
+
+languages=["cpp","java","html","css","python","c","javascript"]
+apple=["swift","thar","audi"]
+print(languages)
+print(apple)
+programming=languages.copy()
+programming=list(languages)
+programming=languages
+print(programming)
+
+all=languages+apple
+print(all)
+all=languages.extend(apple)
+print(all)
