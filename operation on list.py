4871563ee354655a23569d85ed4b7413406191d7
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-10-19 20:07:21 +0530

    operations on list datatype

--- operation on list.py
+++ operation on list.py
@@ -23,3 +23,24 @@ all=languages+apple
 print(all)
 all=languages.extend(apple)
 print(all)
+
+
+my_list = [21, 44, 35, 11]
+for index, val in enumerate(my_list): 
+    print(index, val)
+    
+    
+my_list = [21, 44, 35, 11]
+for index, val in enumerate(my_list, start=1):
+    print(index, val)
+
+
+my_list = [21, 44, 35, 11]
+for index in range(len(my_list)): 
+    value = my_list[index] 
+    print(index, value)
+    
+    
+my_list = [[1], [2, 3], [4, 5, 6, 7]]
+flat_list = [num for sublist in my_list for num in sublist]
+print(flat_list)
