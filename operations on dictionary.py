4aac73e1aae7c01d02ebb0b207dacd45fd52ac1c
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-10-20 19:39:25 +0530

    operations on dictionary

---
+++ operations on dictionary.py
@@ -0,0 +1,19 @@
+shiv={1:'s',2:'h',3:'i',4:'v',5:'a'}
+print(shiv)
+print(shiv[4])
+shiv[2]="xyz"
+print(shiv)
+shiva={'s':1,'h':2,'i':3,'v':4,'a':5}
+print(shiva)
+print(shiva['v'])
+shiva['h']=1234
+print(shiva)
+shiva1={'s':1,'h':2,'i':3,'v':4,'a':5,'s':123,'a':567}
+print(shiva1)
+
+course={"course name":"python","duration":"3 months","fees":"1200 rupees"}
+print(course["course name"])
+print(type(course))
+print(course.get("fees"))
+print(course.keys())
+print(course.values())
