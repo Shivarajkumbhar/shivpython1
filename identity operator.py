85c61799750dad7d4fb3264ccd11ee763040558c
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-11-01 20:31:05 +0530

    identity operator

---
+++ identity operator.py
@@ -0,0 +1,15 @@
+x1=12
+y1=12
+print(x1 is y1)
+print(id(x1))
+print(id(y1))
+x2=[1,2,3]
+y2=[1,2,3]
+print(x2 is y2)
+print(id(x2))
+print(id(y2))
+x3='shivaraj'
+y3='shivaraj'
+print(x3 is not y3)
+print(id(x3))
+print(id(y3))
