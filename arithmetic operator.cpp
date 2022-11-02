80d6d6525496a90439199b34d7e9597b27288a42
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-11-02 19:32:58 +0530

    Create arithmetic operator.cpp

---
+++ arithmetic operator.cpp
@@ -0,0 +1,18 @@
+using namespace std; 
+int main(){
+// declare variables p and q 
+int p = 20, q = 10;
+int result;
+// addition operator 
+result=p+q; 
+cout<<(result);
+// subtraction operator 
+cout<<(p - q);
+/*we can directly perform subtraction in print statement,no need to use result variable here*/
+// multiplication operator 
+cout<<(p * q);
+// division operator 
+cout<<(p / q);
+// modulo operator 
+cout<<(p % q);
+}
