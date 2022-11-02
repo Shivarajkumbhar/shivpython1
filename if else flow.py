5ee606059c9877651f960556969c34b31feda715
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-11-02 19:04:07 +0530

    Create if else flow.py

---
+++ if else flow.py
@@ -0,0 +1,11 @@
+num=3
+if num>0:
+    print('its positive number')
+else:
+    print("its negative number")
+
+num1=-3
+if num1>0:
+    print('its positive number')
+else:
+    print("its negative number")
