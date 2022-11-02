c7ad9be81f46cae58c3c93750b894747ad71b129
Shivarajkumbhar <109192668+Shivarajkumbhar@users.noreply.github.com>
2022-11-02 19:10:33 +0530

    Update if else flow.py

--- if else flow.py
+++ if else flow.py
@@ -16,3 +16,12 @@ if num>0:
     print('its positive number')
 else:
     print("its negative number")
+    
+    
+num=int(input("enter the number :"))
+if num>0:
+    print('its positive number')
+elif num==0:
+    print("its 0 number")
+else:
+    print("its negative number") 
